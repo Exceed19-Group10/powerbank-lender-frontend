e4c132d9db55c26ebc37270c8c966e937d947173
Jeammm <athicha.phjkl@gmail.com>
2023-02-11 15:21:50 +0700

    comment default roue

--- src/services/pw_data.js
+++ src/services/pw_data.js
@@ -1,16 +1,19 @@
 import axios from "axios";
 
+// const default_route = "https://ecourse.cpe.ku.ac.th/exceed10/powerbank"
+const default_route = "http://group10.exceed19.online/powerbank"
+// const default_route = ""http://localhost:3000"
+
+
+
 export async function all_powerbank_status() {
-    // const powerbanks = await axios.get("http://ecourse.cpe.ku.ac.th/exceed10/powerbank/all-powerbank")
-    const powerbanks = await axios.get("http://group10.exceed19.online/powerbank/all-powerbank")
-    // const powerbanks = await axios.get("http://localhost:3000/all_powerbank")
+    const powerbanks = await axios.get(default_route + "/all-powerbank")
     return powerbanks.data
 }
 
 export async function borrow_laew(U_ID, U_PW, PB_ID) {
-    // const res = await axios.put(`http://ecourse.cpe.ku.ac.th/exceed10/powerbank/borrow-laew/${U_ID}/${U_PW}/${PB_ID}`)
     const res = await axios.put(
-        `http://group10.exceed19.online/powerbank/borrow-laew/${PB_ID}`,
+        default_route + `/borrow-laew/${PB_ID}`,
         {
             "user_ID": U_ID,
             "password": U_PW
@@ -19,24 +22,21 @@ export async function borrow_laew(U_ID, U_PW, PB_ID) {
 }
 
 export async function get_powerbank_status(PB_ID) {
-    // const powerbank = await axios.get(`http://ecourse.cpe.ku.ac.th/exceed10/powerbank/get-powerbank/${PB_ID}`)
-    const powerbank = await axios.get(`http://group10.exceed19.online/powerbank/get-powerbank/${PB_ID}`)
-    // const powerbank = await axios.get("http://localhost:3000/get-powerbank")
+    const powerbank = await axios.get(default_route + `/get-powerbank/${PB_ID}`)
     return powerbank.data
 }
 
 export async function confirm(PB_ID) {
-    // const res = await axios.put(`http://ecourse.cpe.ku.ac.th/exceed10/powerbank/confirm-return/${PB_ID}`)
-    const res = await axios.put(`http://group10.exceed19.online/powerbank/check-dai-mai/${PB_ID}`)
+    const res = await axios.put(default_route + `/check-dai-mai/${PB_ID}`)
     return res
 }
 
 export async function force_yu_mai(PB_ID) {
-    const res = await axios.put(`http://group10.exceed19.online/powerbank/return-laew/${PB_ID}`)
+    const res = await axios.put(default_route + `/return-laew/${PB_ID}`)
     return res
 }
 
 export async function get_fee(PB_ID) {
-    const res = await axios.get(`http://group10.exceed19.online/powerbank/fee/${PB_ID}`)
+    const res = await axios.get(default_route + `/fee/${PB_ID}`)
     return res.data
 }
